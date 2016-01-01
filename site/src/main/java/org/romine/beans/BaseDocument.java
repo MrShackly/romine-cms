9de6a02b1b004694baedc4ceeb4cb3da5952ad2b
Aaron Romine <aaron.romine@gmail.com>
2015-12-31 20:55:51 -0500

    Initial Commit

---
+++ site/src/main/java/org/romine/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.romine.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="rominecms:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
